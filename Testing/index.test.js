cc121ebf916b4c0fb73dd68b12fede9295848af6
rheien <anhel_92@hotmail.de>
2021-09-29 14:50:19 +0200

    new modules

--- Testing/index.test.js
+++ Testing/index.test.js
@@ -1,8 +1,9 @@
-/*const responseBuilder=resquire('../src/responseBuilder'),
-const calculateBirthyear=require('../src/calculateBirthyear')
-*/
+const responseBuilder = require("../src/responseBuilder");
+const calculateBirthyear = require("../src/calculateBirthyear");
+const mediaLibrary = require("../src/mediaLibrary");
+
 describe('responseBuilder',()=>{
-    it('beispiel',()=>{
+    /*it('beispiel',()=>{
         let actual=10;
 
         let expected=5;
@@ -10,7 +11,7 @@ describe('responseBuilder',()=>{
         console.log(typeof Nan);
 
         expect(actual).toBeGreaterThan(expected);
-    });
+    });*/
     it('should return code 200 with message',()=>{
         const actual=responseBuilder(200, {message: 'irgendwas'});
 
@@ -20,34 +21,74 @@ describe('responseBuilder',()=>{
         }
         expect(JSON.parse(actual)).toEqual(expected);
     })
-    it('should nreturn code 200 with message and error',()=>{
+    it('should return code 200 with message and error',()=>{
 
-        const expected={
+        /*const expected={
             statusCode:200,
             message:'message',
             error:'error'
-        }
+        }*/
+        const actual = responseBuilder(200, { message: "message", error: "error" });
+
         expect(JSON.parse(actual).message).toBeTruthy();
         expect(JSON.parse(actual).error).toBeTruthy();
     })
 })
 
 describe('calculateBirthyear',()=>{
-    it('should return TypeError when parameter is Object',()=>{
-        const actual=calculateBirthyear({})
+    /*it("should return NaN when parameter is Object", () => {
+    const actual = calculateBirthyear({});
+    expect(actual).toBe(NaN);
+  });*/
 
-        expect(actual).toBe(NaN);
-    })
+  it("should throw TypeError when parameter is NaN", () => {
+    const actual = () => {
+      calculateBirthyear("yeet");
+    };
 
-
-    it('should throw TypeError when parameter is Nan',()=>{
-        const actual=()=>{
-            calculateBirthyear('yeet')
-        }
-        expect(actual).toThrow(TypeError);
-    })
-})
+    expect(actual).toThrow(TypeError);
+  });
+});
 
 describe('mediaLibrary',()=>{
+    it("should add media type to library with value", () => {
+        mediaLibrary.addMediaType("audio", { count: -1 });
     
+        const actual = mediaLibrary.getCountForMediaType("audio");
+    
+        expect(actual).toBe(-1);
+      });
+    
+      it("should delete media type from library", () => {
+        mediaLibrary.deleteMediaType("books");
+    
+        const actual = mediaLibrary.getCountForMediaType("books");
+    
+        expect(actual).toBe(undefined);
+      });
+})
+
+describe("example", () => {
+    beforeAll(() => {
+      jest.spyOn(mediaLibrary, "getCountForMediaType").mockImplementation(() => {
+        return 0;
+      });
+    });
+
+    it("mocking", () => {
+        const fun = () => {
+          const result = mediaLibrary.getCountForMediaType("books");
+    
+          if (result == 0) return true;
+          else return false;
+        };
+    
+        const actual = fun();
+    
+        expect(actual).toBeTruthy();
+      });
+    
+      afterAll(() => {
+        jest.clearAllMocks();
+      }); 
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const bodyparser = require('body-parser');
+const mediaLibrary = require("./mediaLibrary");
+const response = require("./responseBuilder");
 
 const app = express();
 app.use(express.json());
@@ -16,25 +17,23 @@ app.use(function (req, res, next) {
 
 const port = 3000;
 
-const library = {
-    movies: 32,
-    books: 2345678,
-    games: 200
-};
-
-app.get('/healthcheck', (req, res) => res.json({ msg: 'hello world' }));
+app.get('/healthcheck', (req, res) => res.json(response(200, ({ msg: 'läuft' }))));
 
 app.get("/library", (req, res) => {
     const mediaType = req.query.type;
 
-    return res.send(`The media type: ${mediaType} has ${library[mediaType]} units`);
-})
+    return res.json(response(200, {
+        message: `The media type ${mediaType} has ${mediaLibrary.getCountForMediaType(
+          mediaType
+        )} units`,
+      }));
+    });
 
 app.post("/printname", (req, res) => {
     const name = req.body.name
     const age = req.body.age
 
-    return res.send(`name: ${name},age: ${age}`);
+    return res.json(response(`name: ${name},age: ${age}`));
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
